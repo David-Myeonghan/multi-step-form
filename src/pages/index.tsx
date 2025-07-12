b87535b75b162b03950c0e3db7569965916c3026
davidRyu <david.ryu@imagoworks.ai>
2025-07-12 14:17:37 +0900

    step 1 기본 ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import {
   FormControl,
   FormControlLabel,
   FormLabel,
+  Paper,
   Radio,
   RadioGroup,
   Stack,
@@ -15,44 +16,50 @@ import { DatePicker } from '@mui/x-date-pickers';
 export default function Home() {
   return (
     <>
-      <Stack>
-        <Box>
-          <Typography variant="h5">📚 도서 기본 정보</Typography>
-          <Typography variant="subtitle1">Step 1 / 5</Typography>
-          <Typography variant="subtitle1">도서 기본 정보를 입력해주세요.</Typography>
-        </Box>
-        <Box>
-          <TextField sx={{ width: '100%' }} label="책 제목" />
-          <TextField sx={{ width: '100%' }} label="저자" />
-        </Box>
-        <Box>
-          <Typography>독서 상태</Typography>
-          <FormControl>
-            <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
-            <RadioGroup
-              row
-              aria-labelledby="demo-row-radio-buttons-group-label"
-              name="row-radio-buttons-group"
-            >
-              <FormControlLabel value="female" control={<Radio />} label="Female" />
-              <FormControlLabel value="male" control={<Radio />} label="Male" />
-              <FormControlLabel value="other" control={<Radio />} label="Other" />
-              <FormControlLabel value="disabled" disabled control={<Radio />} label="other" />
-            </RadioGroup>
-          </FormControl>
-        </Box>
-        <Stack>
-          <Typography>독서 시작일</Typography>
-          <DatePicker />
-          <Typography>독서 종료일</Typography>
-          <DatePicker />
-
+      <Paper elevation={3} sx={{ p: 3 }}>
+        <Stack gap={3}>
+          {/* Title */}
+          <Box>
+            <Typography variant="h5">📚 도서 기본 정보</Typography>
+            <Typography variant="subtitle1">Step 1 / 5</Typography>
+          </Box>
+          {/* Basic Info */}
+          <Box>
+            <Typography variant="subtitle1">도서 기본 정보를 입력해주세요.</Typography>
+            <TextField sx={{ width: '100%' }} label="책 제목" />
+            <TextField sx={{ width: '100%' }} label="저자" />
+          </Box>
+          {/* Reading Status */}
+          <Box>
+            {/*<Typography>독서 상태</Typography>*/}
+            <FormControl>
+              <FormLabel id="demo-row-radio-buttons-group-label">독서 상태</FormLabel>
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="row-radio-buttons-group"
+              >
+                <FormControlLabel value="female" control={<Radio />} label="읽고싶은 책" />
+                <FormControlLabel value="male" control={<Radio />} label="읽는 중" />
+                <FormControlLabel value="other" control={<Radio />} label="읽음" />
+                <FormControlLabel value="disabled" control={<Radio />} label="보류 중" />
+              </RadioGroup>
+            </FormControl>
+          </Box>
+          {/* Start/End date */}
+          <Stack>
+            <Typography>독서 시작일</Typography>
+            <DatePicker />
+            <Typography>독서 종료일</Typography>
+            <DatePicker />
+          </Stack>
+          {/* Actions */}
+          <Stack direction="row" sx={{ width: '100%' }}>
+            <Button>이전</Button>
+            <Button>다음 ➡</Button>
+          </Stack>
         </Stack>
-        <Stack direction='row' sx={{width: '100%'}}>
-          <Button>이전</Button>
-          <Button>다음 -></Button>
-        </Stack>
-      </Stack>
+      </Paper>
     </>
   );
 }
