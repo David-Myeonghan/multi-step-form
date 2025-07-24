c8edff003cd58d99952f0418ff724302a9bb3b41
David-Myeonghan <myeonghan12@gmail.com>
2025-07-25 01:18:26 +0900

    값 초기화 및 스텝 버튼 클릭시마다 저장

--- src/components/FormAction.tsx
+++ src/components/FormAction.tsx
@@ -1,11 +1,44 @@
+import { useEffect } from 'react';
 import { Button, Stack } from '@mui/material';
 import useStepNavigator from '@/hooks/useStepNavigator';
 import CenterLoading from '@/components/CenterLoading';
+import { useFormContext } from 'react-hook-form';
+import { useAtom } from 'jotai/index';
+import { allFormInfoAtom } from '@/Atom/allFormInfo';
 
 interface FormActionProps {}
 
 export default function FormAction({}: FormActionProps) {
-  const { isLoading, isFirst, isLast, goNext, goPrevious } = useStepNavigator();
+  const { trigger, getValues, reset } = useFormContext();
+  const { isLoading, isFirst, isLast, goPrevious, goNext, stepNumber } = useStepNavigator();
+  const [allFormInfoStorage, setBasicInfo] = useAtom(allFormInfoAtom);
+
+  const handleNextClick = async () => {
+    const isValid = await trigger();
+    if (isValid) {
+      const currentInfo = getValues();
+      setBasicInfo(prev => ({ ...prev, ...currentInfo }));
+      goNext();
+    }
+  };
+
+  const handlePrevClick = () => {
+    const currentInfo = getValues();
+    setBasicInfo(prev => ({ ...prev, ...currentInfo }));
+    goPrevious();
+  };
+
+  const buttonProps = isLast ? { type: 'submit' } : { onClick: handleNextClick };
+
+  useEffect(() => {
+    if (
+      typeof allFormInfoStorage === 'object' &&
+      allFormInfoStorage !== null &&
+      Object.keys(allFormInfoStorage).length > 0
+    ) {
+      reset(allFormInfoStorage);
+    }
+  }, [stepNumber]);
 
   if (isLoading) {
     return <CenterLoading />;
@@ -20,15 +53,20 @@ export default function FormAction({}: FormActionProps) {
     >
       {isFirst === false && (
         <Button
-          onClick={goPrevious}
+          onClick={handlePrevClick}
           variant="contained"
           sx={{ position: 'absolute', bottom: 0, left: 0 }}
         >
           ⬅ 이전
         </Button>
       )}
-      <Button type="submit" variant="contained" sx={{ position: 'absolute', bottom: 0, right: 0 }}>
-        {isLast ? '제출' : `다음 ➡`}
+
+      <Button
+        {...buttonProps}
+        variant="contained"
+        sx={{ position: 'absolute', bottom: 0, right: 0 }}
+      >
+        {isLast ? '제출' : '다음 ➡'}
       </Button>
     </Stack>
   );
