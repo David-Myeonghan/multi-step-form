a3716401a5268ba10938e491ee25959818a687a4
David-Myeonghan <myeonghan12@gmail.com>
2025-07-25 01:22:20 +0900

    불필요 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import { CircularProgress, Paper, Stack } from '@mui/material';
-import React, { useEffect } from 'react';
+import { Paper, Stack } from '@mui/material';
+import React from 'react';
 import BasicInfo from '@/steps/BasicInfo';
 import Recommendation from '@/steps/Recommendation';
 import Review from '@/steps/Review';
@@ -12,8 +12,6 @@ import FormAction from '@/components/FormAction';
 import { useForm } from 'react-hook-form';
 import { BasicInfoFormValues, basicInfoSchema } from '@/schemas/BasicInfoSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useAtom } from 'jotai/index';
-import { basicInfoAtom } from '@/Atom/allFormInfo';
 import RHFProvider from '@/components/RHF/RHFProvider';
 import { schemasByStep } from '@/schemas';
 import CenterLoading from '@/components/CenterLoading';
@@ -40,10 +38,6 @@ export default function Home() {
       return zodResolver(basicInfoSchema)(values, context, options);
     },
   });
-  // const {
-  //   formState: { errors },
-  // } = methods;
-  // console.log(errors);
 
   const handleSubmit = async data => {
     console.log(data);
@@ -77,12 +71,3 @@ export default function Home() {
     </Paper>
   );
 }
-
-// 1 단계:
-// 도서 기본 정보 - 이름, 저자, 출판 년,월,일
-// 독서 상태
-// --> 읽고 싶은 책 / 읽는 중 / 읽음 / 보류중
-// --> 독서(할) 시작일, 독서 종료일 (독서 상태에 종속적)
-
-// todo:
-// how to step(1-5) -> validation -> 각 단계 UI
