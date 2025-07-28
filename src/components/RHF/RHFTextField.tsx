17a61e1d2ac84e9b5219f21e56960a359db61b89
David-Myeonghan <myeonghan12@gmail.com>
2025-07-29 00:36:25 +0900

    TextField 확장

--- src/components/RHF/RHFTextField.tsx
+++ src/components/RHF/RHFTextField.tsx
@@ -5,8 +5,10 @@ interface RHFTextFieldProps {
   name: string;
   label: string;
   sx?: sxProps;
+  multiline?: boolean;
+  rows?: number;
 }
-export default function RHFTextField({ name, label, sx = { width: '100%' } }: RHFTextFieldProps) {
+export default function RHFTextField({ name, label, sx = { width: '100%' }, multiline, rows }: RHFTextFieldProps) {
   const {
     register,
     formState: { errors },
@@ -19,6 +21,8 @@ export default function RHFTextField({ name, label, sx = { width: '100%' } }: RH
       error={!!errors.title}
       helperText={errors.title?.message}
       sx={sx}
+      multiline={multiline}
+      rows={rows}
     />
   );
 }
--- src/steps/Recommendation.tsx
+++ src/steps/Recommendation.tsx
@@ -1,9 +1,21 @@
-import { Box, Stack } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
+import { useFormContext } from 'react-hook-form';
+import RHFTextArea from '@/components/RHF/RHFTextArea';
+import RHFTextField from '@/components/RHF/RHFTextField';
 
 export default function Recommendation() {
+  const { control } = useFormContext();
+
   return (
     <Stack gap={2}>
       <Box gap={2}>Recommendation Contents</Box>
+
+      <Stack>
+        <Box>
+          <Typography>코멘트</Typography>
+          <RHFTextField label="코멘트" name="review" multiline={true} rows={5} />
+        </Box>
+      </Stack>
     </Stack>
   );
 }
