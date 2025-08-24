2926eebbf65783a0c6fe179ca09f194b7c256798
David-Myeonghan <myeonghan12@gmail.com>
2025-08-24 23:42:48 +0900

    스키마 추가

--- src/schemas/quoteSharingSchema.ts
+++ src/schemas/quoteSharingSchema.ts
@@ -1,2 +1,20 @@
 import { z } from 'zod';
-export const quoteSharingSchema = z.object({});
+export const quoteSharingSchema = z
+  .object({
+    quotation: z.string().optional(),
+    quotationPage: z.number().min(1, '1 이상의 페이지 번호를 입력해주세요'),
+    totalPage: z.number().min(1, '1 이상의 페이지 번호를 입력해주세요'),
+    isPublic: z.boolean(),
+  })
+  .refine(
+    data => {
+      if (data.quotation && data.quotation.trim() !== '') {
+        return data.quotationPage <= data.totalPage;
+      }
+      return true;
+    },
+    {
+      message: '인용구 페이지 번호는 전체 페이지 수보다 적거나 같아야 합니다.',
+      path: ['quotationPage'],
+    },
+  );
