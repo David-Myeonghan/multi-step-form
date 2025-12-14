fdc2624efe75cdc69049965a3b663f1e4dc10497
David-Myeonghan <myeonghan12@gmail.com>
2025-12-14 20:15:22 +0900

    Dayjs 직렬화/역직렬화 로직 추가

--- src/Atom/allFormInfo.ts
+++ src/Atom/allFormInfo.ts
@@ -1,7 +1,27 @@
 import { atomWithStorage, createJSONStorage } from 'jotai/utils';
 import { MultiStepFormValues } from '@/schemas';
+import dayjs, { Dayjs } from 'dayjs';
 
-const storage = createJSONStorage<Partial<MultiStepFormValues>>(() => sessionStorage);
+const storage = createJSONStorage<Partial<MultiStepFormValues>>(() => sessionStorage, {
+  reviver: (key, value) => {
+    // 날짜 문자열을 Dayjs 객체로 변환
+    if (
+      typeof value === 'string' &&
+      (key === 'publishedAt' || key === 'readingStartedAt' || key === 'readingFinishedAt')
+    ) {
+      return dayjs(value);
+    }
+    return value;
+  },
+  replacer: (_key, value) => {
+    // Dayjs 객체를 ISO 문자열로 변환
+    if (dayjs.isDayjs(value)) {
+      return (value as Dayjs).toISOString();
+    }
+    return value;
+  },
+});
+
 export const allFormInfoAtom = atomWithStorage<Partial<MultiStepFormValues>>(
   'allFormInfo',
   {},
